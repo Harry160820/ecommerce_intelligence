b78a6e267c5b7ea14b51ed44af8e006dafd359dc
Harry160820 <hs103868@gmail.com>
2026-04-06 15:59:08 +0530

    eda done

---
+++ src/train_churn.py
@@ -0,0 +1,7 @@
+"""
+XGBoost churn classifier.
+Logs metrics and artifacts to MLflow.
+Writes predictions back to PostgreSQL (ml_churn_predictions table).
+ 
+From notebook:  from src.train_churn import train_churn_model
+"""
